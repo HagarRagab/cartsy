f6e6d64102e925ac632258793fd7030a1ac098c4
HagarRagab <101262480+HagarRagab@users.noreply.github.com>
2026-02-10 15:22:22 +0200

    update ProductsContainer.js

--- src/app/_components/header/CartIcon.js
+++ src/app/_components/header/CartIcon.js
@@ -19,7 +19,7 @@ function CartIcon({ numCartItems, locale }) {
     return (
         <Link href={`/${locale}/cart`} className="relative cursor-pointer">
             <ShoppingCart size={24} />
-            <p className="absolute bottom-4 left-3 right-0 bg-primary-200 text-text-200 rounded-full w-5 h-5 flex items-center justify-center border-1 border-bg-800 text-sm">
+            <p className="absolute bottom-4 left-3 right-0 bg-primary-200 text-text-200 rounded-full w-5 h-5 flex items-center justify-center border border-bg-800 text-sm">
                 {orderSummary.totalCartItems}
             </p>
         </Link>
--- src/app/_components/shared/ProductsContainer.js
+++ src/app/_components/shared/ProductsContainer.js
@@ -2,7 +2,7 @@ import ProductCard from "@/src/app/_components/shared/ProductCard";
 
 function ProductsContainer({ products }) {
     return (
-        <div className="grid grid-cols-[repeat(auto-fit,minmax(150px,220px))] gap-x-3 gap-y-8 pb-8 mx-auto">
+        <div className="grid grid-cols-[repeat(auto-fit,minmax(150px,220px))] justify-center gap-x-3 gap-y-8 pb-8 mx-auto">
             {products.map((product) => (
                 <ProductCard
                     key={product.id}
